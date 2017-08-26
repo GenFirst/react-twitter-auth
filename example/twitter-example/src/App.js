750776f3a4385800794c5499616049b9b139d696
Ivan Vasiljevic <robince885@gmail.com>
2017-08-26 02:16:22 +0200

    Add demo project

---
+++ example/twitter-example/src/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import TwitterLogin from 'react-twitter-login/src/index';
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <TwitterLogin/>
+      </div>
+    );
+  }
+}
+
+export default App;
